93bb645d5a78bc46c54b3c6527f382a790655bda
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2026-03-10 15:48:36 +0100

    some cleaning and running rstudio

---
+++ run_rstudio.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -f "$HOME/rstudio.sif" ]; then
+    echo "container already exists, skipping download"
+else
+    singularity pull $HOME/rstudio.sif docker://rocker/tidyverse:4
+fi
+
+SIF=$HOME/rstudio.sif
+
+# specify path to tmp directory created in previous section
+TMPDIR=$HOME/rstudio-tmp
+
+if [ -d "$TMPDIR" ]; then
+    echo "tmp directory already exists, skipping creation"
+else
+    mkdir -p "$TMPDIR/var/lib"
+    mkdir -p "$TMPDIR/var/run"
+    mkdir -p "$TMPDIR/tmp"
+    mkdir -p "$TMPDIR/home"
+fi
+
+singularity exec \
+    --bind /etc/passwd:/etc/passwd \
+    --bind /etc/group:/etc/group \
+    --workdir $TMPDIR/home \
+    -B $TMPDIR/var/lib:/var/lib/rstudio-server \
+    -B $TMPDIR/var/run:/var/run/rstudio-server \
+    -B $TMPDIR/tmp:/tmp \
+    $SIF \
+    rserver --www-address=127.0.0.1 --server-user=$(whoami)
