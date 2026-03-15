ae5d8abd529f691b6f58ebf86aa32fd5c7fcf364
Toni Hermoso Pulido <toniher@cau.cat>
2026-03-15 21:31:18 +0100

    Handling datetime

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import datetime
 import json
 import os
 
@@ -31,11 +32,24 @@ html_context = {
     "matomo_site_id": matomo_site_id,
 }
 
+# Handling bioschemas
+def convert_dates(obj):
+    if isinstance(obj, dict):
+        return {k: convert_dates(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_dates(i) for i in obj]
+    elif isinstance(obj, (datetime.date, datetime.datetime)):
+        return obj.isoformat()
+    else:
+        return obj
+
+
 bioschemas_jsonld = None
 yaml_path = os.path.join(os.path.dirname(__file__), "bioschemas.yaml")
 if os.path.exists(yaml_path):
     with open(yaml_path, "r") as f:
         bioschemas_data = yaml.safe_load(f)
+    bioschemas_data = convert_dates(bioschemas_data)
     bioschemas_jsonld = json.dumps(bioschemas_data, indent=2)
     html_context["bioschemas_jsonld"] = bioschemas_jsonld
 
